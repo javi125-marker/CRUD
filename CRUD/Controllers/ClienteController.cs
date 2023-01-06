be1748826cb1b99e011f5e2506755b5bcd334a70
admin <admin@DESKTOP-MTB7TFC>
2023-01-06 07:18:15 -0500

    Agregar archivos de proyecto.

---
+++ CRUD/Controllers/ClienteController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+using CRUD.Models.ViewModels;
+
+
+namespace CRUD.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            //Extraemos la lista del ViewModels
+            List<ListClienteViewModel> lst;
+
+            //Contexto para la conexion a la Bdd
+            using(facturaEntities2 db = new facturaEntities2())
+            {
+                //llenado de lista
+                lst = (from d in db.cliente
+                       select new ListClienteViewModel
+                       {
+                           id_cli = d.id_cli,
+                           nombre_cli = d.nombre_cli,
+                           correo_cli = d.correo_cli
+                       }).ToList();
+            }
+            return View(lst);
+        }
+        //Metodo para la accion Nuevo retorna a la vista
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        //Metodo donde le pasamos el modelo
+        [HttpPost]
+        public ActionResult Nuevo(ClienteViewModel clientemodel)
+        {
+            try
+            {
+                //Validar los data Annotations
+                if (ModelState.IsValid)
+                {
+                    //Si todo es valido vamos a guardar los datos en la base
+                    using(facturaEntities2 db = new facturaEntities2())
+                    {
+                        var oCliente = new cliente();
+                        oCliente.id_cli = clientemodel.id_cli;
+                        oCliente.nombre_cli = clientemodel.nombre_cli;
+                        oCliente.cedula_cli = clientemodel.cedula_cli;
+                        oCliente.correo_cli = clientemodel.correo_cli;
+                        oCliente.fechaNacimiento_cli = clientemodel.fechaNacimiento_cli;
+
+                        //Se añade y guradan los datos en la BDD
+                        db.cliente.Add(oCliente);
+                        db.SaveChanges();
+                    }
+                    
+                }
+                return Redirect("~/Cliente/Index");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ CRUD/Models/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models.ViewModels
+{
+    public class ClienteViewModel
+    {
+        public int id_cli { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        [StringLength(50)]
+        public string nombre_cli { get; set; }
+        [Required]
+        [Display(Name = "Cedula")]
+        [StringLength(10)]
+        public string cedula_cli { get; set; }
+        [Required]
+        [Display(Name = "Correo")]
+        [StringLength(80)]
+        [EmailAddress]
+        public string correo_cli { get; set; }
+        [Required]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime fechaNacimiento_cli { get; set; }
+
+    }
+}
